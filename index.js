399778d5e5deb8e0a2b590d904268a59433a71e9
sancoza <sandrabalog@rocketmail.com>
2022-10-11 08:57:37 +0200

    1 file modified

--- index.js
+++ index.js
@@ -140,3 +140,31 @@
 // console.log(true || false); true
 // console.log(false || true); true
 // console.log(false || false); *****false****
+
+// If the variable age stores the information about user’s age, how we can check whether it
+// contains a correct value? Write expressions for checking:
+// ● Is age negative value
+// ● Is age greater than 120
+
+var age = 35;
+
+// if ( age > 120) {
+//   console.log('Ima više od 120 godina');
+// } else {
+//   console.log('Ima manje od 120 godina');
+// }
+// age();
+
+// if ( age < 0) {
+//   conosle.log('Age je negativan broj');
+// } else {
+//   console.log('Age je pozitivan broj');
+// }
+// age();
+
+// Let’s say there are speed limitations on a motorway from 60 to 120 kilometers per hour. If
+// we store the current speed value in the variable speed. Write an expression which will check if
+// we are driving drive safely or not? (true if we are driving safe and false if not)
+
+
+
