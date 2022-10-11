0481a4cdf9db20739f71ccfa75ff35cee57069f2
sancoza <sandrabalog@rocketmail.com>
2022-10-11 11:31:44 +0200

    1 file modified

--- index.js
+++ index.js
@@ -162,20 +162,18 @@
 // }
 // age();
 
-// Let’s say there are speed limitations on a motorway from 60 to 120 kilometers per hour. If
-// we store the current speed value in the variable speed. Write an expression which will check if
-// we are driving drive safely or not? (true if we are driving safe and false if not)
+//
 
-// var driverSpeed = 30;
-// var minspeedLimit =60;
-// var maxspeedLimit =90;
+var driverSpeed = 30;
+var minspeedLimit =60;
+var maxspeedLimit =90;
 
-// if(driverSpeed <= minspeedLimit || driverSpeed >= maxspeedLimit ) {
-//   console.log('Nezadovoljavajuća brzina - false')
-// }else {
-//   console.log('Bravo bravo voziš pravo -true')
-// }
-
+if(driverSpeed < minspeedLimit || driverSpeed > maxspeedLimit ) {
+  console.log('Nezadovoljavajuća brzina - false')
+}else {
+  console.log('Bravo bravo voziš pravo -true')
+}
+driverSpeed();
 
 
 
