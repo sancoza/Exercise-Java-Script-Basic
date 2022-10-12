f5083a61f42756845d6ca6996e492b6ce740758a
sancoza <sandrabalog@rocketmail.com>
2022-10-12 10:07:39 +0200

    1 file modified

--- index.js
+++ index.js
@@ -141,5 +141,19 @@
 //   console.log('this sign is -');
 // }
 
-// 5. Write a program to check if the variable is a number. If it’s a number, check if it is divisible by 2. If it is, print the result, if not, show “X” Sample numbers: 10 | 7 (check one at the time)
-// Output: 10 / 2 = 5 | X
+// // 5. Write a program to check if the variable is a number. If it’s a number, check if it is divisible by 2.
+// //  If it is, print the result, if not, show “X” Sample numbers: 10 | 7 (check one at the time)
+// // Output: 10 / 2 = 5 | X
+
+// var number = 10;
+// var result = number / 2;
+
+// if(10 % 2 === 0) {
+//   console.log(result);
+// }else{
+//   console.log('X');
+// }
+
+// 6.Write a conditional statement to find the largest of five numbers. Display the result in console.
+// Sample numbers: -5, -2, -6, 0, -1
+// Output: 0
