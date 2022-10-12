7663346836ba44252fd0d23635215d76072583bb
sancoza <sandrabalog@rocketmail.com>
2022-10-12 07:58:17 +0200

    1 file modified, 2 added, 2 deleted

---
+++ index.js
@@ -0,0 +1,75 @@
+// // conditions
+// // ***  if
+// // var result = '';
+
+// // var a = 3;
+
+// // if(a > 2) {
+// //   result = a + ' is greater than 2';
+// // }
+// // console.log(result);
+
+// // *** else
+// var result = '';
+
+// a = 3;
+
+// if (a > 2) {
+//   result = a + ' is greater then 2';
+// } else {
+//   result = a + ' is not gretaer then 2';
+// }
+// console.log(result);
+
+// var result = '';
+
+// var b = 1;
+
+// if (b > 2) {
+//   result = b + ' is greater then 2';
+// } else {
+//   result = b + ' is not greater then 2';
+// }
+// console.log(result);
+
+// var result = '';
+// c = 0;
+// d = 1;
+
+// // *** else if
+// if (c > 2 || c < -2) {
+//   result = ' c is not between -2 and 2 ';
+// } else if (c === 0 && d === 0) {
+//   result = ' both are zero';
+// }else if(c === d) {
+//   result=' c and d is equal';
+// }else {
+//   result= 'I give up'
+// }
+// console.log(result);
+
+// var result ='';
+// f = 1;
+// e=3;
+
+// if( f === 1) {
+//   if(e === 2) {
+//     result=' f is 1 e is 2';
+//   }else {
+//     result=' f is 1 but e is not 2 ';
+//   }
+// }else {
+//   result='No result'
+// }
+// console.log(result);
+
+// //*** Checking if a Variable Exists
+
+// result='';
+
+// if (typeof house !== "undefined") {
+//   result='yes';
+// }
+// console.log(result);
+
+
